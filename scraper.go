44a2d990c9ccdc31d2e06c981da7367c16828b25
pedrsrs <pedrosrs.dev@gmail.com>
2024-07-06 18:07:59 -0300

    refactoring

--- scraper.go
+++ scraper.go
@@ -14,29 +14,40 @@ import (
 	"github.com/gocolly/colly"
 )
 
-var BOOK_LIMIT int = 100
-var NUM_SCRAPERS int = 10
-var book_count int = 0
+const (
+	BOOKS_DIR     = "./books"
+	BOOK_LIMIT    = 1000
+	NUM_WORKERS   = 3
+	MAX_RETRIES   = 3
+	RETRY_DELAY   = 2 * time.Second
+	REQUEST_DELAY = 100 * time.Millisecond
+)
 
-var mutex = &sync.Mutex{}
+var (
+	book_count int
+	mutex      sync.Mutex
+	sem        = make(chan struct{}, NUM_WORKERS)
+	quit       = make(chan bool)
+	once       sync.Once
+)
 
-func scrapeLinks(summary_links []string, wg *sync.WaitGroup, book_links chan<- []string) {
+func create_books_dir() {
+	if _, err := os.Stat(BOOKS_DIR); os.IsNotExist(err) {
+		os.Mkdir(BOOKS_DIR, 0755)
+	}
+}
+
+func scrape_links(summary_links []string, wg *sync.WaitGroup, book_links chan<- []string) {
 	defer wg.Done()
 
 	var collected_book_links []string
-
 	c := colly.NewCollector()
 
 	c.OnHTML("h2", func(e *colly.HTMLElement) {
-		linkText := strings.TrimSpace(e.Text)
-		if strings.Contains(linkText, "(English)") {
+		link_text := strings.TrimSpace(e.Text)
+		if strings.Contains(link_text, "(English)") {
 			e.DOM.Find("a[href]").Each(func(_ int, sel *goquery.Selection) {
-				if book_count >= BOOK_LIMIT {
-					return
-				}
 				link, _ := sel.Attr("href")
-				book_count = book_count + 1
-				fmt.Println(book_count)
 				collected_book_links = append(collected_book_links, link)
 			})
 		}
@@ -49,9 +60,8 @@ func scrapeLinks(summary_links []string, wg *sync.WaitGroup, book_links chan<- [
 	book_links <- collected_book_links
 }
 
-func get_indexes_links() []string {
+func get_indexes_link_list() []string {
 	c := colly.NewCollector()
-
 	var scraped_links []string
 
 	c.OnHTML("div.pgdbnavbar > p:nth-of-type(2) > a", func(e *colly.HTMLElement) {
@@ -64,119 +74,137 @@ func get_indexes_links() []string {
 	return scraped_links
 }
 
-func divide_index_links(indexes_link_list []string) [][]string {
-	portionSize := (len(indexes_link_list) + NUM_SCRAPERS - 1) / NUM_SCRAPERS
+func divide_indexes_link_list(indexes_links_list []string) [][]string {
+	portion_size := (len(indexes_links_list) + NUM_WORKERS - 1) / NUM_WORKERS
 	var portions [][]string
 
-	for i := 0; i < len(indexes_link_list); i += portionSize {
-		end := i + portionSize
-		if end > len(indexes_link_list) {
-			end = len(indexes_link_list)
+	for i := 0; i < len(indexes_links_list); i += portion_size {
+		end := i + portion_size
+		if end > len(indexes_links_list) {
+			end = len(indexes_links_list)
 		}
-		portions = append(portions, indexes_link_list[i:end])
+		portions = append(portions, indexes_links_list[i:end])
 	}
 
 	return portions
 }
 
-func extract_book_ids(book_links []string) map[string]string {
-	bookIDToLink := make(map[string]string)
+func extractbook_ids(book_links []string) map[string]string {
+	book_info := make(map[string]string)
 
 	for _, book_link := range book_links {
-		lastSlashIndex := strings.LastIndex(book_link, "/")
-		if lastSlashIndex != -1 {
-			book_id := book_link[lastSlashIndex+1:]
+		last_slash_index := strings.LastIndex(book_link, "/")
+		if last_slash_index != -1 {
+			book_id := book_link[last_slash_index+1:]
 			formatted_link := fmt.Sprintf("https://www.gutenberg.org/cache/epub/%s/pg%s.txt", book_id, book_id)
-			bookIDToLink[book_id] = formatted_link
+			book_info[book_id] = formatted_link
 		}
 	}
 
-	return bookIDToLink
+	return book_info
 }
 
-func fetchAndSaveBooks(links map[string]string) {
+func fetch_books(links map[string]string) {
 	var wg sync.WaitGroup
 
-	for bookID, link := range links {
+	for book_id, link := range links {
 		wg.Add(1)
-		go func(bookID, link string) {
+		go func(book_id, link string) {
 			defer wg.Done()
+			sem <- struct{}{}
+			defer func() { <-sem }()
 
-			resp, err := http.Get(link)
-			if err != nil {
-				fmt.Printf("Error fetching book %s: %v\n", bookID, err)
-				time.Sleep(1 * time.Second)
-				return
-			}
-			defer resp.Body.Close()
-
-			// Read response body
-			body, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				fmt.Printf("Error reading response body for book %s: %v\n", bookID, err)
-				time.Sleep(1 * time.Second)
-				return
-			}
-
-			// Save to file
-			mutex.Lock()
-			defer mutex.Unlock()
-
-			dir := "./books"
-			if _, err := os.Stat(dir); os.IsNotExist(err) {
-				err := os.Mkdir(dir, 0755)
-				if err != nil {
-					fmt.Printf("Error creating directory %s: %v\n", dir, err)
-					return
+			if fetch_and_save_books(book_id, link) {
+				mutex.Lock()
+				book_count++
+				if book_count >= BOOK_LIMIT {
+					once.Do(func() { close(quit) })
 				}
+				mutex.Unlock()
 			}
-
-			filePath := filepath.Join(dir, fmt.Sprintf("%s.txt", bookID))
-			err = ioutil.WriteFile(filePath, body, 0644)
-			if err != nil {
-				fmt.Printf("Error writing file %s: %v\n", filePath, err)
-				return
-			}
-
-			fmt.Printf("Book %s saved successfully.\n", bookID)
-
-			// Introduce a delay after each request
-			time.Sleep(1 * time.Second)
-		}(bookID, link)
+		}(book_id, link)
 	}
 
 	wg.Wait()
 }
 
-func main() {
-	var indexes_link_list []string = get_indexes_links()
+func fetch_and_save_books(book_id, link string) bool {
+	for retry := 0; retry < MAX_RETRIES; retry++ {
+		select {
+		case <-quit:
+			return false
+		default:
+			time.Sleep(REQUEST_DELAY)
+			resp, err := http.Get(link)
+			if err != nil {
+				fmt.Printf("Error fetching book %s (attempt %d): %v\n", book_id, retry+1, err)
+				time.Sleep(RETRY_DELAY)
+				continue
+			}
+			defer resp.Body.Close()
 
-	var portions [][]string = divide_index_links(indexes_link_list)
-	var wg sync.WaitGroup
+			if resp.StatusCode == http.StatusNotFound {
+				fmt.Printf("Book %s not found (404).\n", book_id)
+				return false
+			} else if resp.StatusCode != http.StatusOK {
+				fmt.Printf("Error fetching book %s: HTTP status %s\n", book_id, resp.Status)
+				time.Sleep(RETRY_DELAY)
+				continue
+			}
 
-	book_links := make(chan []string, NUM_SCRAPERS)
-
-	// Start scrapers
-	for _, portion := range portions {
-		wg.Add(1)
-		go scrapeLinks(portion, &wg, book_links)
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				fmt.Printf("Error reading response body for book %s: %v\n", book_id, err)
+				time.Sleep(RETRY_DELAY)
+				continue
+			}
+			if BOOK_LIMIT > book_count {
+				save_book_file(book_id, body)
+				fmt.Printf("Book %s saved successfully.\n", book_id)
+			}
+			return true
+		}
 	}
 
-	// Wait for all goroutines to finish
+	fmt.Printf("Failed to fetch and save book %s after %d retries.\n", book_id, MAX_RETRIES)
+	return false
+}
+
+func save_book_file(file_name string, content []byte) error {
+	file_path := filepath.Join(BOOKS_DIR, fmt.Sprintf("%s.txt", file_name))
+	err := ioutil.WriteFile(file_path, content, 0644)
+	if err != nil {
+		return fmt.Errorf("error writing file %s: %v", file_path, err)
+	}
+
+	return nil
+}
+
+func main() {
+	indexes_links_list := get_indexes_link_list()
+	portions := divide_indexes_link_list(indexes_links_list)
+	var wg sync.WaitGroup
+
+	create_books_dir()
+
+	book_links := make(chan []string, NUM_WORKERS)
+
+	for _, portion := range portions {
+		wg.Add(1)
+		go scrape_links(portion, &wg, book_links)
+	}
+
 	go func() {
 		wg.Wait()
 		close(book_links)
 	}()
 
-	// Collect all results
-	var allLinks []string
+	var all_book_links []string
 	for link := range book_links {
-		allLinks = append(allLinks, link...)
+		all_book_links = append(all_book_links, link...)
 	}
 
-	// Extract book IDs and formatted links
-	bookIDToLink := extract_book_ids(allLinks)
+	book_info := extractbook_ids(all_book_links)
 
-	// Fetch and save books
-	fetchAndSaveBooks(bookIDToLink)
+	fetch_books(book_info)
 }
